2fdf8a090dbb7040f5d4ffcb0445a9fe218e46b2
Alejandro Miguel Cisneros Martínez <cima@ciencias.unam.mx>
2023-06-05 16:06:10 -0600

    Create genbank2five-column.sh

---
+++ genbank2five-column.sh
@@ -0,0 +1,20 @@
+#USAGE: create a list.txt of .gbk files
+# for i in $(cat list.txt ); do echo ">Feature ""${i%.gbk}"; genbank2five-column.sh $i; done
+
+grep -E 'CDS|product' $1 |
+sed '/translation/d' |
+sed 's/  *//g' |
+sed 's/CDS//' |
+sed 's/\.\./	/' |
+awk '{ if ($1 ~ /complement/) {t=$1; $1=$2; $2=t; print} else {print}}' |
+sed 's/  */	/' |
+sed 's/calpro/cal pro/' |
+sed 's/="/	/' |
+tr -d '"' |
+tr -d '>' |
+tr -d '<' |
+gsed '1~2 s/$/	CDS/g' |
+sed 's/complement//' |
+tr -d ')' |
+tr -d '(' |
+sed 's/\//			/'
